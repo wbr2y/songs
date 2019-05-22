949517f39df29cb35f1afb0b0234537358501280
Willian Barros <wbr2y@apatria.com.br>
2019-05-22 13:15:37 -0400

    Time to break!

---
+++ src/components/SongList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class SongList extends React.Component {
+	render() {
+		return <div>SongList</div>
+	}
+}
+
+export default connect()(SongList)
